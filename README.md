e82a87e689132e34907904d170e4337dec84e4fe
shunakiya <shunakiya@gmail.com>
2024-10-26 19:27:31 -0700

    minor fixes (still on progress)

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ Welcome to my portfolio page! It's built using:
 - Tailwind
 - React-icons
 
-As a disclaimer, my portfolio was **heavily** inspired by [Brittany Chiang's portfolio](https://brittanychiang.com/)
+As a disclaimer, my portfolio was **heavily** inspired by:
+
+- [Brittany Chiang](https://brittanychiang.com/)
+- [Oskar Glen](https://oskarfranttiglen.com/)
+
+If you have any questions, feel free to [shoot me an email!](mailto:shunakiya@gmail.com)
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
+import pfp from "./assets/pfp.png";
 
 export default function App() {
   const writeEmail = () => {
@@ -8,48 +9,59 @@ export default function App() {
   };
 
   return (
-    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
-      {/* header */}
-      <div className="sticky flex flex-col py-24">
-        <h1 className="text-5xl font-bold">Shunsuke Akiya</h1>
+    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-8 font-sans md:px-12 md:py-20 lg:px-16 lg:py-0">
+      <div className="py-20 h-screen">
+        {/* header */}
+        <div className="sticky flex flex-col">
+          <img src={pfp} className="w-36 h-36 rounded-full mb-3" />
+          <h1 className="text-4xl font-bold">Shunsuke Akiya</h1>
 
-        <div className="mt-3">
-          <p className="text-xl font-medium mb-4">
-            Fullstack{""}
-            <span className="animate-text-gradient bg-gradient-to-r from-purple-400 via-pink-500 to-blue-400 bg-[200%_auto] bg-clip-text text-transparent mx-1">
-              React
-            </span>
-            {""}
-            Developer
-          </p>
+          <div className="mt-3">
+            <p className="text-xl font-medium mb-3">
+              <span className="animate-text-gradient bg-gradient-to-r from-purple-400 via-pink-500 to-blue-400 bg-[200%_auto] bg-clip-text text-transparent mr-1">
+                Full-Stack
+              </span>
+              {""}
+              Developer
+            </p>
 
-          <p className="text-lg w-[335px]">
-            I build scalable, user-friendly applications across front and back
-            end.
-          </p>
+            <div className="flex flex-row">
+              <p className="text-[18px] mb-3 -ml-[8.5px]">
+                📍La Quinta, California
+              </p>
+              <p className="mt-[2px] ml-[6px]">🇺🇸</p>
+            </div>
+
+            <p className="text-[16px] w-[335px] text-gray-400">
+              I build scalable, user-friendly applications across front and back
+              end.
+            </p>
+          </div>
         </div>
-      </div>
 
-      {/* nav buttons */}
-      <div className="flex flex-col items-start tracking-wider">
-        <button>ABOUT</button>
+        {/* nav buttons */}
+        <div className="flex flex-col items-start tracking-widest my-auto">
+          <button>About</button>
 
-        <button>PROJECTS</button>
-      </div>
+          <button>Experience</button>
 
-      {/* socials */}
-      <div className="flex items-center">
-        <div className="flex space-x-5 items-center">
-          <a href="https://github.com/shunakiya" target="_blank">
-            <FaGithub size={30} />
-          </a>
+          <button>Projects</button>
+        </div>
 
-          <a href="https://linkedin.com/in/shunsuke-akiya/" target="_blank">
-            <FaLinkedin size={30} />
-          </a>
-          <button onClick={writeEmail}>
-            <IoIosMail size={40} />
-          </button>
+        {/* socials */}
+        <div className="flex items-center bottom-0">
+          <div className="flex space-x-5 items-center">
+            <a href="https://github.com/shunakiya" target="_blank">
+              <FaGithub size={30} />
+            </a>
+
+            <a href="https://linkedin.com/in/shunsuke-akiya/" target="_blank">
+              <FaLinkedin size={30} />
+            </a>
+            <button onClick={writeEmail}>
+              <IoIosMail size={40} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
