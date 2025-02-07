7cc9c9467ad9eda09634db7fd63aa9c7bff5b371
shunakiya <shunakiya@gmail.com>
2025-02-06 16:00:37 -0800

    refactoring skills, experience and projects

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,7 @@
 import { FaReact } from "react-icons/fa";
 import { SiTypescript, SiMysql, SiFlask } from "react-icons/si";
-import { RiTailwindCssFill, RiNextjsFill } from "react-icons/ri";
+import { RiTailwindCssFill } from "react-icons/ri";
+// import { SkillsList, skillsListItems } from "../utils/SkillsList";
 import figma from "../assets/logos/figma.svg";
 import python from "../assets/logos/python.svg";
 import svelte from "../assets/logos/svelte.svg";
@@ -9,6 +10,18 @@ export default function Skills() {
   return (
     <div className="flex flex-col mb-1">
       <h1 className="text-2xl font-semibold mb-1.5">Skills</h1>
+
+      {/* <>
+        <div>
+          {skillsListItems.map((item: SkillsList, index: number) => (
+            <div key={index}>
+              {item.title}
+              {item.description}
+              {item.color}
+            </div>
+          ))}
+        </div>
+      </> */}
 
       <div className="grid grid-cols-2 grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 md:grid-cols-3 md:grid-rows-2 sm:grid-cols-2 sm:grid-rows-3 gap-4">
         <div className="group flex flex-row items-center bg-slate-800 p-2 rounded-lg px-3 w-[168px] lg:w-52 md:w-52 sm:w-52 py-2.5 border border-transparent transition-all duration-150 ease-in-out hover:border-slate-600">
@@ -50,24 +63,6 @@ export default function Skills() {
             <p className="text-[11px] lg:text-[10px] md:text-[10px] sm:text-[11px] text-slate-500 transition-colors duration-150 ease-in-out group-hover:text-[#a4b4cb]">
               CSS Framework
             </p>
-          </div>
-        </div>
-
-        <div className="relative group flex flex-row items-center bg-slate-800 p-2 rounded-lg px-3 w-[168px] lg:w-52 md:w-52 sm:w-52 py-2.5 border border-transparent transition-all duration-150 ease-in-out hover:border-slate-600">
-          <div className="bg-[#d8e1e230] w-11 h-11 justify-center flex items-center rounded-lg">
-            <RiNextjsFill size={28} className="fill-[#d8e1e2]" />
-          </div>
-          <div className="flex flex-col mx-2 ml-2.5">
-            <p className="text-base mb-0.5 text-[#a4b4cb] transition-colors duration-150 ease-in-out group-hover:text-[#e6f0ff]">
-              Next.js
-            </p>
-            <p className="text-[11px] lg:text-[10px] md:text-[10px] sm:text-[11px] text-slate-500 transition-colors duration-150 ease-in-out group-hover:text-[#a4b4cb]">
-              React Framework
-            </p>
-          </div>
-
-          <div className="absolute top-1 right-1 bg-yellow-500 text-[7px] lg:text-[10px] md:text-[10px] sm:text-[7px] text-slate-700 px-1.5 py-0.5 rounded-lg">
-            In Progress
           </div>
         </div>
 
---
+++ src/utils/ProjectsList.ts
@@ -0,0 +1,8 @@
+export interface ProjectsList {
+  logo: string;
+  title: string;
+  time: string;
+  desc1: string;
+  desc2: string;
+  descr3: string;
+}
