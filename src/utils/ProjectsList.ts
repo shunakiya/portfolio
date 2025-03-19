4b4114186733d3c84667486861e88daee5f8361b
shunakiya <shunakiya@gmail.com>
2025-03-19 11:23:42 -0700

    updated resume and salsnb description/logo

--- src/utils/ProjectsList.ts
+++ src/utils/ProjectsList.ts
@@ -1,5 +1,6 @@
 import csusb from "../assets/logos/csusb.png";
 import ponder from "../assets/logos/ponder.png";
+import open from "../assets/logos/open.png";
 
 export interface ProjectsList {
   logo: string;
@@ -27,17 +28,17 @@ export const projectsListItems: ProjectsList[] = [
       "Developed with React, TypeScript, and Tailwind CSS; future iteration aims to migrate to Next.js, and potentially add Supabase or MongoDB.",
   },
   {
-    logo: csusb,
+    logo: open,
     title: "Secure Access Locking Sytem with NFC and Biometrics",
     location: "California State University, San Bernardino",
     time: "Fall 2024 - Present",
-    repoPage: "https://github.com/shunakiya/salsnb",
+    repoPage: "https://un-lock.vercel.app",
     desc1:
-      "Developed a smart-lock with fingerprint sensor, NFC, and key access.",
+      "Developed a user-friendly web app using Next.js, TypeScript, and Tailwind CSS that interfaces with a Raspberry Pi to control a smart lock.",
     desc2:
-      "Integrated remote control, solenoid-powered lock, fingerprint reader, and smartphone NFC system.",
+      "Engineered the authentication system for the lock using MongoDB to manage user credentials, along with secure API communication between a front-end and a back-end for fingerprint and NFC access control.",
     desc3:
-      "Used a Raspberry Pi to host a Flask backend server, with data fetched by a React frontend designed with Tailwind.",
+      "Integrated a multi-platform system with a modern web interface using Tailwind CSS, ensuring a seamless and intuitive UX.",
   },
   {
     logo: csusb,
